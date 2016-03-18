a853f0dbc86d5c014bf9a597345735006c34cb6c
Sunnah <suh3@hi.is>
2016-03-18 13:49:15 +0000

    heh eyddi ovart ollu ut, vona thetta komi nuna!

---
+++ pakki/FlightSearch.java
@@ -0,0 +1,34 @@
+package pakki;
+//í db: availableSeats, arrivalAirport, departureAirport, arrivalTime departureTime date? flightId
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
+import pakki.DataBaseConnection;
+import pakki.Flight;
+
+public class FlightSearch{
+	DataBaseConnection dbc=new DataBaseConnection();
+	public FlightSearch(){
+		
+	}
+	//Veit ekki ennþá hvernig við höfum tímasetningar og hvernig við afmörkum tímann, einnig veit ekki hvernig við fáum niðurstöðurnar
+	//spurning að hafa date og svo arrivalTime og departurTime, þa date er dagsetning og hitt eru tímasetningar
+	public List<Flight>search(int peopleCount, String arrivalTime, String aA, String dA){
+		ResultSet rs= dbc.getFromDB("Select * from flights WHERE availableSeats>=peopleCount and aA=arrivalAirport and da=departureAirport");
+		List<Flight> list = new ArrayList<Flight>();
+		Flight f=null;
+		try{
+			while(rs.next()){
+				f=new Flight(rs.getString("arrivalTime"), rs.getString("departureTime"), rs.getString("arrivalAirport"), rs.getString("departureAirport"), rs.getInt("price"));
+				list.add(f);
+				
+			}
+		}
+	    catch(Exception e) {
+	    	e.printStackTrace();
+	    	System.err.println(e.getClass().getName()+": "+e.getMessage());
+	    	System.exit(0);
+	    }
+		return list;
+	}
+}
---
+++ pakki/tester.java
@@ -0,0 +1,9 @@
+package pakki;
+public class tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
