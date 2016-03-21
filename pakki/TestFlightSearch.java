f37bbd911da06bf0936d03e493a70f25b0664303
Sunnah <suh3@hi.is>
2016-03-21 14:32:37 +0000

    enntha i testerunum

---
+++ pakki/TestFlightSearch.java
@@ -0,0 +1,25 @@
+package pakki;
+
+import static org.junit.Assert.*;
+import pakki.FlightSearch;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestFlightSearch {
+	private FlightSearch fs;
+	@Before
+	public void setUp() throws Exception {
+				fs=new FlightSearch();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSerch() {
+		
+	}
+
+}
