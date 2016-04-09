de44232adb9ef65581125cd306bb829d04e2397c
Unnur Kristín <unnkbry@gmail.com>
2016-04-09 15:04:13 +0000

    Bæti við FlightSearchResult

---
+++ pakki/FlightSearchResult.java
@@ -0,0 +1,49 @@
+package pakki;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import java.awt.BorderLayout;
+
+public class FlightSearchResult {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FlightSearchResult window = new FlightSearchResult();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FlightSearchResult() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		table = new JTable();
+		frame.getContentPane().add(table, BorderLayout.NORTH);
+	}
+
+}
