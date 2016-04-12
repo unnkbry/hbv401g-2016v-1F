20da9312e8c89b0bab0a3df89a2ff1ace98dd5b5
ragnheidursv <ragnh94@gmail.com>
2016-04-12 14:03:46 +0000

    Classes to send email: Email.java and EmailSender.java added. NullPointerException

---
+++ pakki/Email.java
@@ -0,0 +1,15 @@
+package pakki;
+
+public class Email {
+	public static void main(String[] args){
+		String[] to = {"ragnh94@gmail.com"};
+		EmailSender.sendMail("dreamteam.flightservice@gmail.com", "0412952019", "message to reciepents", args);
+		if(EmailSender.sendMail
+				("dreamteam.flightservice@gmail.com", 
+						"0412952019", 
+						"message to reciepents", to)) {
+			System.out.println("email sent successful");
+		}
+		else System.out.println("Some error occured");	
+	}
+}
